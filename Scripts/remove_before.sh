0ed858b89e12af7889e4651be4b5118777abc89f
lsmin1021 <39587551+lsmin1021@users.noreply.github.com>
2021-01-29 00:25:38 +0900

    Update remove_before.sh

--- Scripts/remove_before.sh
+++ Scripts/remove_before.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find /opt/codedeploy-agent/deployment-root/c7694906-c580-4c28-82fc-7c9101b17b18/* -maxdepth 0 -type 'd' | grep -v $(stat -c '%Y:%n' /opt/codedeploy-agent/deployment-root/c7694906-c580-4c28-82fc-7c9101b17b18/* | sort -t: -n | tail -1 | cut -d: -f2- | cut -c 3-) | xargs rm -rf
+find /opt/codedeploy-agent/deployment-root/edf98d7c-86e6-4c76-a1ba-b8fd289803a2/* -maxdepth 0 -type 'd' | grep -v $(stat -c '%Y:%n' /opt/codedeploy-agent/deployment-root/edf98d7c-86e6-4c76-a1ba-b8fd289803a2/* | sort -t: -n | tail -1 | cut -d: -f2- | cut -c 3-) | xargs rm -rf
 sudo rm -rf /home/ec2-user/*
